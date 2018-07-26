0e76b9c03aff9132e29d78bf02c6c458b47d4641
Duncan McDonald <dmcdonald9000@gmail.com>
2018-07-26 18:13:40 -0400

    improved responsive design for mobile and comment to js file

--- js/fanvote.js
+++ js/fanvote.js
@@ -1,5 +1,7 @@
+//cheeky alert function//
+
 function nope() {
   debugger;
   alert("Sorry the ranking system is perfect, not accepting " + document.getElementById("player_one").value + " in our Top 5");
-  
+
 }
